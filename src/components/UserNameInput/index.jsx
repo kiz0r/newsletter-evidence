2bfca2822bdf617005159a8e09ab0119507903c4
kiz0r <kkhomenko10@gmail.com>
2023-04-06 11:47:09 +0200

    install classnames, sass, gh-pages and set homepage in package.json

--- src/components/UserNameInput/index.jsx
+++ src/components/UserNameInput/index.jsx
@@ -1,9 +1,14 @@
 import { useState } from 'react';
+import { classNames } from 'classnames';
+
+import styles from 'UserNameInput.module.sass';
 
 const USERNAME_REG_EXP = /[A-Z][a-z]{2,19}(?=[A-Z][a-z]{2,19})/;
 
 const UserNameInput = () => {
   const [value, setValue] = useState('');
+
+  const inputClassname = classNames(userNameInput);
 
   return (
     <input
