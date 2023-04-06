1df1ada6259550a3b29eaa7c5d558773d374297c
kiz0r <kkhomenko10@gmail.com>
2023-04-06 13:23:08 +0200

    create additive styles using sass mixin

--- src/components/NewsletterEvidence/index.jsx
+++ src/components/NewsletterEvidence/index.jsx
@@ -1,7 +1,14 @@
 import UserNameInput from './../UserNameInput';
 
+import styles from './../../common/styles/NewsletterEvidence.module.sass';
+const { evidence } = styles;
+
 const NewsletterEvidence = () => {
-  return <UserNameInput />;
+  return (
+    <div className={evidence}>
+      <UserNameInput />
+    </div>
+  );
 };
 
 export default NewsletterEvidence;
--- src/components/UserNameInput/index.jsx
+++ src/components/UserNameInput/index.jsx
@@ -1,8 +1,8 @@
 import { useState, useEffect } from 'react';
 import classNames from 'classnames';
 
-import styles from './UserNameInput.module.sass';
-const { userNameInput, inputValid, inputInvalid } = styles;
+import styles from './../../common/styles/NewsletterEvidence.module.sass';
+const { userNameInput, inputValid, inputInvalid, formLabel } = styles;
 
 const USERNAME_REG_EXP =
   /^[A-Z][a-z]{1,19}(-[A-Z][a-z]{1,19})?\s[A-Z][a-z]{1,19}(-[A-Z][a-z]{1,19})?$/;
@@ -25,15 +25,17 @@ const UserNameInput = () => {
   );
 
   return (
-    <input
-      className={inputClassname}
-      type="text"
-      value={value}
-      onChange={({ target: { value } }) => setValue(value)}
-      placeholder="UserName"
-      autoFocus
-      required
-    />
+    <label htmlFor="" className={formLabel}>
+      <span>Your Name </span>
+      <input
+        className={inputClassname}
+        type="text"
+        value={value}
+        onChange={({ target: { value } }) => setValue(value)}
+        autoFocus
+        required
+      />
+    </label>
   );
 };
 
