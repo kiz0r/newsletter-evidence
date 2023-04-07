88c3b7c99e4e0ff86f272da6e830de31df206b53
kiz0r <kkhomenko10@gmail.com>
2023-04-07 14:57:03 +0200

    celebrating sour victory

--- src/App.js
+++ src/App.js
@@ -2,6 +2,21 @@ import PageHeader from './components/PageHeader';
 import PageMain from './components/PageMain';
 import NewsletterEvidence from './components/NewsletterEvidence';
 import PageFooter from './components/PageFooter';
+import { FaGithub, FaLinkedin, FaTelegramPlane } from 'react-icons/fa';
+
+const SOCIAL_LINKS = [
+  {
+    name: 'LinkedIn',
+    link: 'https://www.linkedin.com/in/khomenkokyrylo/',
+    icon: <FaLinkedin />,
+  },
+  { name: 'GitHub', link: 'https://github.com/kiz0r', icon: <FaGithub /> },
+  {
+    name: 'Telegram',
+    link: 'https://t.me/khomenkodev',
+    icon: <FaTelegramPlane />,
+  },
+];
 
 function App() {
   return (
@@ -10,7 +25,16 @@ function App() {
       <PageMain>
         <NewsletterEvidence />
       </PageMain>
-      <PageFooter>Kyrylo Khomenko </PageFooter>
+      <PageFooter>
+        <p>Kyrylo Khomenko</p>
+        <ul>
+          {SOCIAL_LINKS.map(({ name, link, icon }) => (
+            <li key={name}>
+              <a href={link}>{icon}</a>
+            </li>
+          ))}
+        </ul>
+      </PageFooter>
     </>
   );
 }
